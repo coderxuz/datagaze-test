2ac7af84fd359f58d8bca1ae448635c89ab55c52
xursand <148965057+coderxuz@users.noreply.github.com>
2025-01-31 11:45:29 +0000

    weather model

--- app/models.py
+++ app/models.py
@@ -10,5 +10,21 @@ class User(Base):
     username: Mapped[str] =mapped_column(unique=True) 
     password: Mapped[str]
 
+    
+class WeatherData(Base):
+    __tablename__ = 'weather_data'
+    
+    name: Mapped[str]
+    country: Mapped[str]
+    lat: Mapped[float]
+    lon: Mapped[float]
+    temp_c: Mapped[float]
+    temp_color: Mapped[str]
+    wind_kph: Mapped[float]
+    wind_color: Mapped[str]
+    cloud: Mapped[int]
+    cloud_color: Mapped[str]
+    created_at:Mapped[datetime]
+
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
